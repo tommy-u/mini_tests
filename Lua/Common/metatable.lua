f6b76e9530dbaae25e90d8e2705fc2a2fda83bff
Tommy Unger <tunger10@gmail.com>
2016-06-23 17:16:24 -0400

    metatable ex

--- Lua/Common/metatable.lua
+++ Lua/Common/metatable.lua
@@ -1,4 +1,5 @@
-http://lua-users.org/wiki/MetamethodsTutorial
+--http://lua-users.org/wiki/MetamethodsTutorial
+
 local func_example = setmetatable( {}, {__index = function(t,k)
 						     return "key doesn't exist"
 						  end})
@@ -25,3 +26,28 @@ local m = setmetatable({}, {__newindex = function (table, key, value)
 m[123] = 456
 print(m[123]) --> nil
 print(t[123]) --> 456
+
+--https://github.com/torch/demos/blob/master/train-a-digit-classifier/dataset-mnist.lua
+
+local labelvector = torch.zeros(10)
+
+local mt = 
+{ 
+__index = function(self, index)
+	     local input = self.data[index]
+	     local class = self.labels[index]
+	     local label = labelvector:zero()
+	     label[class] = 1 -- onehot
+	     return {input, label}
+	  end
+}
+
+mydat = {}
+mydat.data = {1,2,3}
+mydat.labels = {4,5,6}
+
+setmetatable(mydat, mt)
+
+print(mydat[2])
+print(mydat[2][1])
+print(mydat[2][2])
